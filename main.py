4aeab8c68c27424e28890897a310132c9fb41426
Christopher Pereyda <christopher.pereyda@gmail.com>
2019-04-28 15:08:38 -0700

    l

--- main.py
+++ main.py
@@ -18,11 +18,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 from keras.datasets import mnist
 from keras.layers import Input, Dense, Flatten
 from keras.models import Model, Sequential
+from keras import backend as K
 
+from numpy.random import seed
+from tensorflow import set_random_seed
+
+from numba import cuda
+
+seed(3)
+set_random_seed(4)
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+import csv
 
 class Test:
 
@@ -45,11 +55,15 @@ class AE:
         self.A = A
         self.tolerance = tolerance
 
-        
+        t1 = Test(self.testA.size + self.testA.size, None)
+        t1.combine(self.testA.data, self.testA.data)
+
+        t2 = Test(self.testB.size + self.testB.size, None)
+        t2.combine(self.testB.data, self.testB.data)
         
         # Perform search
-        x1 = self.search(self.testA)
-        x2 = self.search(self.testB)
+        x1 = self.search(t1)
+        x2 = self.search(t2)
 
         # Output sofar
         print('x1:', x1, ' x2:', x2)
@@ -60,6 +74,7 @@ class AE:
 
         # Search combination of test
         x3 = self.search(t)
+
 
         # Return results
         return (x1, x2, x3)
@@ -90,6 +105,8 @@ class AE:
             x = int((high + low)/2)
             model = self.gen_model(test, x)
             result = self.eval(test, model)
+            
+
 
             # Check result
             if result < self.A:
@@ -101,29 +118,56 @@ class AE:
             if high - low <= self.tolerance:
                 done = True
 
+            
+            del result
+            del model
+
         return x
 
 
     def eval(self, test, model):
-        history = model.fit(test.data, test.data, epochs=10, batch_size=10, validation_split=0.1, verbose=0)
+        history = model.fit(test.data, test.data, epochs=12, batch_size=10, validation_split=0.1, verbose=0,shuffle=True)
 
         n = max(history.history['acc'])
         print(n)
         return n
-        
+
+
+
+# Logging utility
+# Expects a results history
+def fancy_logger(x1, x2, x3, overlap, file_name='data', write='a'):
+    with open(file_name + '.csv', write, newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow((x1, x2, x3, overlap))
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train1 = x_train[0:1000] / 255
-x_train2 = x_train[500:1500] / 255
+
+#print(y_train[0:100])
+        
+for i in range(2,11):
+
+    x_train1 = x_train[0:1000] / 255
+    x_train2 = x_train[i * 100: i * 100 + 1000] / 255
+
+    print(x_train1.shape)
+    print(x_train2.shape)
 
 
-x_train1 = x_train1.reshape((1000, 784))
-x_train2 = x_train2.reshape((1000, 784))
+    x_train1 = x_train1.reshape((1000, 784))
+    x_train2 = x_train2.reshape((1000, 784))
 
-t1 = Test(784, x_train1)
-t2 = Test(784, x_train2)
+    t1 = Test(784, x_train1)
+    t2 = Test(784, x_train2)
 
-hel = AE(t1,t2)
-res = hel.run(0.80, 1)
+    hel = AE(t1,t2)
+    res = hel.run(0.815, 1)
+    x1, x2, x3 = res
+    fancy_logger(x1, x2, x3, 1.0 - i/10.0, file_name='data')
 
+    
+    # Release memory
+    K.clear_session()
+    #exit()
 print(res)
