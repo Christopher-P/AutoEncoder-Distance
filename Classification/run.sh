9ab3f8ff03f0abe4f7f6b0d83876921ac97defac
Christopher Pereyda <christopher.pereyda@gmail.com>
2019-09-05 12:07:11 -0700

    Update

---
+++ Classification/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for j in 0 1 2 3 4 
+do
+   for i in 0 1 2 3 4 5 6 7 8 9 10
+   do
+      python simple-class.py $i $j
+   done
+done
+
+
--- Classification/simple-class.py
+++ Classification/simple-class.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Hello World!
 
 #TODO: Test similarity between [0] to [1] and [1] to [0]
@@ -15,7 +16,7 @@ presented tests!
 import os
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
-from keras.datasets import mnist, cifar10
+from keras.datasets import mnist, cifar10, cifar100, fashion_mnist
 from keras.layers import Input, Dense, Flatten, Conv2D, Activation, BatchNormalization, MaxPooling2D, Dropout
 from keras.models import Model, Sequential
 from keras import backend as K
@@ -35,6 +36,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import csv
+import sys
 
 # https://appliedmachinelearning.blog/2018/03/24/achieving-90-accuracy-in-object-recognition-task-on-cifar-10-dataset-with-keras-convolutional-neural-networks/
 
@@ -82,14 +84,18 @@ def fancy_logger(x1, overlap, file_name='data', write='a'):
         spamwriter.writerow((x1, overlap))
 
 def T_A_B(m_data, c_data, i):
-    m_data[0] = np.pad(m_data[0], pad_width=((0,0),(2,2),(2,2),(0,0)), mode='constant', constant_values='0.0')
-    m_data[2] = np.pad(m_data[2], pad_width=((0,0),(2,2),(2,2),(0,0)), mode='constant', constant_values='0.0')
-
     m_data[0] = m_data[0][0:50000]
     m_data[1] = m_data[1][0:50000]
     m_data[2] = m_data[2][0:50000]
     m_data[3] = m_data[3][0:50000]
 
+    c_data[0] = c_data[0][0:50000]
+    c_data[1] = c_data[1][0:50000]
+    c_data[2] = c_data[2][0:50000]
+    c_data[3] = c_data[3][0:50000]
+    
+    l1 = len(c_data[1][0])
+    l2 = len(m_data[1][0])
     n_m = 50000
     n_c = 50000
 
@@ -107,32 +113,38 @@ def T_A_B(m_data, c_data, i):
     c_data[3] = c_data[3][0:frac_c]
     print(frac_m, frac_c)
 
-    c_data[1] = np.pad(c_data[1], pad_width=((0,0),(0,10)), mode='constant', constant_values='0.0')
-    c_data[3] = np.pad(c_data[3], pad_width=((0,0),(0,10)), mode='constant', constant_values='0.0')
+    c_data[1] = np.pad(c_data[1], pad_width=((0,0),(0, l2)), mode='constant', constant_values='0.0')
+    c_data[3] = np.pad(c_data[3], pad_width=((0,0),(0, l2)), mode='constant', constant_values='0.0')
 
-    m_data[1] = np.pad(m_data[1], pad_width=((0,0),(10,0)), mode='constant', constant_values='0.0')
-    m_data[3] = np.pad(m_data[3], pad_width=((0,0),(10,0)), mode='constant', constant_values='0.0')
+    m_data[1] = np.pad(m_data[1], pad_width=((0,0),(l1, 0)), mode='constant', constant_values='0.0')
+    m_data[3] = np.pad(m_data[3], pad_width=((0,0),(l1, 0)), mode='constant', constant_values='0.0')
 
     c_data[0] = np.concatenate((c_data[0], m_data[0]),axis=0)
     c_data[1] = np.concatenate((c_data[1], m_data[1]),axis=0)
     c_data[2] = np.concatenate((c_data[2], m_data[2]),axis=0)
     c_data[3] = np.concatenate((c_data[3], m_data[3]),axis=0)
 
-    model = gen_model((32,32,1), 20)
+    model = gen_model((32,32,1), l1 + l2)
     hist = model.fit(c_data[0], c_data[1], epochs=10, validation_data=(c_data[2], c_data[3]), batch_size=32, validation_split=0.1, verbose=2, shuffle=True)
     res = max(hist.history['val_acc'])
     return res
     
 
 def main():
-
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
 
     (x_train_m, y_train_m), (x_test_m, y_test_m) = mnist.load_data()
     (x_train_c, y_train_c), (x_test_c, y_test_c) = cifar10.load_data()
+    (x_train_c2, y_train_c2), (x_test_c2, y_test_c2) = cifar100.load_data(label_mode='fine')
+    (x_train_f, y_train_f), (x_test_f, y_test_f) = fashion_mnist.load_data()
 
     m_data = [x_train_m, y_train_m, x_test_m, y_test_m]
+    f_data = [x_train_f, y_train_f, x_test_f, y_test_f]
     c_data = [x_train_c, y_train_c, x_test_c, y_test_c]
+    c2_data = [x_train_c2, y_train_c2, x_test_c2, y_test_c2]
 
+    ## MNIST
     m_data[0] = m_data[0] / 255
     m_data[2] = m_data[2] / 255
 
@@ -142,6 +154,23 @@ def main():
     m_data[1] = to_categorical(m_data[1])
     m_data[3] = to_categorical(m_data[3])
 
+    m_data[0] = np.pad(m_data[0], pad_width=((0,0),(2,2),(2,2),(0,0)), mode='constant', constant_values='0.0')
+    m_data[2] = np.pad(m_data[2], pad_width=((0,0),(2,2),(2,2),(0,0)), mode='constant', constant_values='0.0')
+    
+    ## FMNIST
+    f_data[0] = f_data[0] / 255
+    f_data[2] = f_data[2] / 255
+
+    f_data[0] = f_data[0].reshape((60000, 28, 28, 1))
+    f_data[2] = f_data[2].reshape((10000, 28, 28, 1))
+
+    f_data[1] = to_categorical(f_data[1])
+    f_data[3] = to_categorical(f_data[3])
+
+    f_data[0] = np.pad(f_data[0], pad_width=((0,0),(2,2),(2,2),(0,0)), mode='constant', constant_values='0.0')
+    f_data[2] = np.pad(f_data[2], pad_width=((0,0),(2,2),(2,2),(0,0)), mode='constant', constant_values='0.0')
+
+    ## CIFAR10
     c_data[0] = c_data[0].dot([0.2126, 0.7152, 0.0722]) / 255
     c_data[2] = c_data[2].dot([0.2989, 0.5870, 0.1140]) / 255
 
@@ -151,11 +180,35 @@ def main():
     c_data[1] = to_categorical(c_data[1])
     c_data[3] = to_categorical(c_data[3])
 
-    for i in range(7,11):
 
-        r1 = T_A_B(copy.deepcopy(m_data), copy.deepcopy(c_data), i/10)
+    ## CIFAR100
+    c2_data[0] = c2_data[0].dot([0.2126, 0.7152, 0.0722]) / 255
+    c2_data[2] = c2_data[2].dot([0.2989, 0.5870, 0.1140]) / 255
 
-        fancy_logger(r1, i / 10)
+    c2_data[0] = c2_data[0].reshape((50000, 32, 32, 1))
+    c2_data[2] = c2_data[2].reshape((10000, 32, 32, 1))
+
+    c2_data[1] = to_categorical(c2_data[1])
+    c2_data[3] = to_categorical(c2_data[3])
+
+    if b == 0:
+        c = m_data
+        d = f_data
+    elif b == 1:
+        c = m_data
+        d = c2_data
+    elif b == 2:
+        c = f_data
+        d = c_data
+    elif b == 3:
+        c = f_data
+        d = c2_data
+    elif b == 4:
+        c = c_data
+        d = c2_data
+
+    r1 = T_A_B(copy.deepcopy(c), copy.deepcopy(d), a/10)
+    fancy_logger(r1, a / 10)
 
 
 
